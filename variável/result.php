875d72fa8def8572e9ce4bdbc64ee92ef6fa0420
VALDELI DE JESUS <152724145+valderbill@users.noreply.github.com>
2025-02-25 19:25:55 -0300

    Add files via upload

---
+++ variável/result.php
@@ -0,0 +1,14 @@
+<?php
+
+
+    // declaração de variável
+    $nome = "Valdeli de Jesus";
+    $idade = 48;
+
+
+    // saída de dados
+    echo "<b>Nome do desenvolvedor:</b> " . $nome . ".<br />";
+    echo "<b>Idade: </b> " . $idade . " anos.";
+
+
+?>
