aa9af2112f50b8aea803051136235492754bc487
VALDELI DE JESUS <152724145+valderbill@users.noreply.github.com>
2025-03-09 16:45:57 -0300

    Add files via upload

---
+++ funcao1/index.php
@@ -0,0 +1,12 @@
+<?php
+
+// função
+function darBoasVindas() {
+    // escrever as ações da função
+    $nomeProgramador = "Valdeli de Jesus";
+    echo "Seja bem vindo, " . $nomeProgramador . ".";
+}
+// executando a função
+darBoasVindas();
+
+?>
